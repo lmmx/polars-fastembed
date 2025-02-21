f3d0ef12df800e5ec945158e096ebe0ffc1b29fe
Louis Maddox <louismmx@gmail.com>
2025-02-21 15:36:59 +0000

    build: update build deps

--- rewrite/src/lib.rs
+++ rewrite/src/lib.rs
@@ -1,7 +1,13 @@
+use pyo3_polars::PolarsAllocator;
 use pyo3::prelude::*;
+
+mod expressions;
 
 #[pymodule]
 fn _polars_fastembed(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add("__version__", env!("CARGO_PKG_VERSION"))?;
     Ok(())
 }
+
+#[global_allocator]
+static ALLOC: PolarsAllocator = PolarsAllocator::new();

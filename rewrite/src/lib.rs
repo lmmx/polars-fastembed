f3d3f42d4324e0d64c6bf37d37cd19b948ea3636
Louis Maddox <louismmx@gmail.com>
2025-02-22 16:38:03 +0000

    fix(cibuildwheel): attempt to fix CI build on aarch64 as described in https://github.com/PyO3/maturin-action/discussions/162#discussioncomment-7978369

--- rewrite/src/lib.rs
+++ rewrite/src/lib.rs
@@ -1,12 +1,30 @@
-use pyo3_polars::PolarsAllocator;
+// See discussion 162
+// https://github.com/PyO3/maturin-action/discussions/162#discussioncomment-7978369
+#[cfg(features = "openssl-vendored")]
+use openssl_probe;
+
 use pyo3::prelude::*;
+use pyo3_polars::PolarsAllocator;
 
 mod expressions;
 mod model_suggestions;
 mod registry;
 
+// See discussion 162
+#[cfg(features = "openssl-vendored")]
+fn probe_ssl_certs() {
+    openssl_probe::init_ssl_cert_env_vars();
+}
+
+// See discussion 162
+#[cfg(not(openssl_probe))]
+fn probe_ssl_certs() {}
+
 #[pymodule]
 fn _polars_fastembed(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    // See discussion 162
+    probe_ssl_certs();
+
     m.add("__version__", env!("CARGO_PKG_VERSION"))?;
     m.add_function(wrap_pyfunction!(registry::register_model, m)?)?;
     m.add_function(wrap_pyfunction!(registry::clear_registry, m)?)?;

8f028177f0d5a8c79496042951208514fd40fe05
Louis Maddox <lmmx@users.noreply.github.com>
2025-02-27 11:01:59 +0000

    FEAT(x86): build ORT from source (#8)
    * chore: re-enable Linux x86 build
    
    * build: try a before script
    
    * build: Python 3.8 and flatbuffers
    
    * fix: typo
    
    * fix: more flags
    
    * build: try passing container
    
    * build: fix
    
    * revert: failed
    
    * build: trace the build script
    
    * build(trace): pysnooper is my quant
    
    * fix: typo
    
    * build: bump trace depth
    
    * fix: guess that we are missing install/
    
    * revert: remove install flags
    
    * build: env vars
    
    * build: scl toolchain
    
    * build: EPEL
    
    * build: symlink to 32 bit gcc
    
    * fix: OpenSSL 32 bit
    
    * build: openssl and pass dockeropts more neatly
    
    * fix: stupid string quotes
    
    * build(i686): fix 32-bit `ort` build by specifying `-m32` flags and i686 OpenSSL, adjust `PKG_CONFIG_PATH` and `LIBRARY_PATH`
    
    * chore: give up
    
    * build: try to combine (if only to leave in a better state to return to)
    
    * fix(typo): stupid LLM-induced typo
    
    * build: try removing 64 bit openssl-devel and 'Development Tools' group (just in case)
    
    * fix(cmake-m32): install `libstdcc++-static.i686`
    
    * fix: use shared-libstdc++
    
    * build: get whatprovides libstdc++ nonshared
    
    * build: gcc toolset
    
    * build: remove shared flag
    
    * build: disable AVX2 (used in MLAS)
    
    * build: use toolchain
    
    * fix: stupid typo
    
    * fix: typo
    
    * fix: crate the symlink in PATH
    
    * build: debug i686-linux-gnu-gcc not found error
    
    * build: confirm source of i686 gcc
    
    * debug: no symlink
    
    * build: cc-rs flags
    
    * fix: abspath gcc
    
    * debug: no gcc at symlink
    
    * fix: gcc path link
    
    * fix: readlink on .so
    
    * fix: more paths
    
    * build: verbose linker flags
    
    * fix: spaces separate the flag
    
    * build: try another rustflags fix
    
    * build: pass rust flags via toml
    
    * build(optimise): use Release config, no debug info
    
    * chore(check-commit): turn off building on every PR commit

---
+++ scripts/i686-toolchain.cmake
@@ -0,0 +1,10 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR i686)
+
+set(CMAKE_C_COMPILER gcc)
+set(CMAKE_CXX_COMPILER g++)
+set(CMAKE_ASM_COMPILER gcc)
+
+set(CMAKE_C_FLAGS "-m32 -msse2")
+set(CMAKE_CXX_FLAGS "-m32 -msse2")
+set(CMAKE_ASM_FLAGS "-m32 -msse2")

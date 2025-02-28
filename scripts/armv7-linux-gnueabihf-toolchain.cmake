bba2e1ca42b9f6563c030d54016dce606fa9363f
Louis Maddox <lmmx@users.noreply.github.com>
2025-02-28 19:36:15 +0000

    build(toolchain): Create armv7-linux-gnueabihf-toolchain.cmake

---
+++ scripts/armv7-linux-gnueabihf-toolchain.cmake
@@ -0,0 +1,16 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+
+set(CMAKE_C_COMPILER arm-linux-gnueabihf-gcc)
+set(CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++)
+set(CMAKE_ASM_COMPILER arm-linux-gnueabihf-gcc)
+
+set(CMAKE_C_FLAGS "-march=armv7")
+set(CMAKE_CXX_FLAGS "-march=armv7")
+
+set(CMAKE_FIND_ROOT_PATH /usr/arm-linux-gnueabihf)
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
